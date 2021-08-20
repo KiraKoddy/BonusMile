29446e4d575f44dfc4b828429b2f24d2081ba192
Kira Koddy <flatcher2007@yandex.ru>
2021-08-20 16:26:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 800;
+        int mile = 20;
+        int bonusMile = price / mile;
+        System.out.println("Бонусные мили: " + bonusMile);
+    }
+}
